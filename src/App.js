eeaac25547a9bae36443ec0653cfa09de11e5e41
Mohammad Sadman <41444793+kaneki666@users.noreply.github.com>
2020-04-09 15:50:46 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 
-function App() {
+// core components
+import Admin from "./layouts/Admin.js";
+import RTL from "./layouts/RTL.js";
+
+import "./assets/css/material-dashboard-react.css?v=1.8.0";
+
+const App = () => {
+  const hist = createBrowserHistory();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={hist}>
+      <Switch>
+        <Route path="/admin" component={Admin} />
+        <Route path="/rtl" component={RTL} />
+        <Redirect from="/" to="/admin/dashboard" />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
